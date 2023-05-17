6948ec2e07c0ef2da10b603cf45c1877b5f3bc5b
ciminoce <ciminoce@yahoo.com.ar>
2023-05-17 10:05:06 -0300

    Se agregaron formularios al proyecto para codificar mas adelante

---
+++ Jardines2023.Windows/frmProductoAE.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Jardines2023.Windows
+{
+    partial class frmProductoAE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.chkSuspendido = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nudMinimo = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nudStock = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboProveedores = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboCategorias = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrecioVta = new System.Windows.Forms.TextBox();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinimo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(581, 183);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(159, 52);
+            this.btnBuscar.TabIndex = 81;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnBuscar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(665, 278);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 66);
+            this.btnCancelar.TabIndex = 80;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(44, 278);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 66);
+            this.btnOK.TabIndex = 82;
+            this.btnOK.Text = "OK";
+            this.btnOK.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnOK.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImagen.Location = new System.Drawing.Point(581, 32);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(159, 143);
+            this.pbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagen.TabIndex = 79;
+            this.pbImagen.TabStop = false;
+            // 
+            // chkSuspendido
+            // 
+            this.chkSuspendido.AutoSize = true;
+            this.chkSuspendido.Location = new System.Drawing.Point(144, 235);
+            this.chkSuspendido.Name = "chkSuspendido";
+            this.chkSuspendido.Size = new System.Drawing.Size(15, 14);
+            this.chkSuspendido.TabIndex = 78;
+            this.chkSuspendido.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 235);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 77;
+            this.label7.Text = "Suspendido:";
+            // 
+            // nudMinimo
+            // 
+            this.nudMinimo.Location = new System.Drawing.Point(144, 200);
+            this.nudMinimo.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudMinimo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudMinimo.Name = "nudMinimo";
+            this.nudMinimo.ReadOnly = true;
+            this.nudMinimo.Size = new System.Drawing.Size(120, 20);
+            this.nudMinimo.TabIndex = 75;
+            this.nudMinimo.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 72;
+            this.label6.Text = "Stock Mínimo:";
+            // 
+            // nudStock
+            // 
+            this.nudStock.Location = new System.Drawing.Point(144, 166);
+            this.nudStock.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudStock.Name = "nudStock";
+            this.nudStock.ReadOnly = true;
+            this.nudStock.Size = new System.Drawing.Size(120, 20);
+            this.nudStock.TabIndex = 76;
+            this.nudStock.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 73;
+            this.label4.Text = "Stock:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 74;
+            this.label3.Text = "Precio Vta:";
+            // 
+            // cboProveedores
+            // 
+            this.cboProveedores.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboProveedores.FormattingEnabled = true;
+            this.cboProveedores.Location = new System.Drawing.Point(144, 99);
+            this.cboProveedores.Name = "cboProveedores";
+            this.cboProveedores.Size = new System.Drawing.Size(263, 21);
+            this.cboProveedores.TabIndex = 70;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 67;
+            this.label5.Text = "Proveedor:";
+            // 
+            // cboCategorias
+            // 
+            this.cboCategorias.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCategorias.FormattingEnabled = true;
+            this.cboCategorias.Location = new System.Drawing.Point(144, 65);
+            this.cboCategorias.Name = "cboCategorias";
+            this.cboCategorias.Size = new System.Drawing.Size(263, 21);
+            this.cboCategorias.TabIndex = 71;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 68;
+            this.label2.Text = "Categoría:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 69;
+            this.label1.Text = "Producto:";
+            // 
+            // txtPrecioVta
+            // 
+            this.txtPrecioVta.Location = new System.Drawing.Point(144, 133);
+            this.txtPrecioVta.MaxLength = 200;
+            this.txtPrecioVta.Name = "txtPrecioVta";
+            this.txtPrecioVta.Size = new System.Drawing.Size(263, 20);
+            this.txtPrecioVta.TabIndex = 65;
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(144, 32);
+            this.txtProducto.MaxLength = 200;
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(360, 20);
+            this.txtProducto.TabIndex = 66;
+            // 
+            // frmProductoAE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 367);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.pbImagen);
+            this.Controls.Add(this.chkSuspendido);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.nudMinimo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nudStock);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboProveedores);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cboCategorias);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrecioVta);
+            this.Controls.Add(this.txtProducto);
+            this.Name = "frmProductoAE";
+            this.Text = "frmProductoAE";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinimo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.PictureBox pbImagen;
+        private System.Windows.Forms.CheckBox chkSuspendido;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nudMinimo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown nudStock;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboProveedores;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboCategorias;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrecioVta;
+        private System.Windows.Forms.TextBox txtProducto;
+    }
+}
